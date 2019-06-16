8b13a57438566eee4e083382e39cf403d16c1d5e
Beniamin Bia <biabeniamin@outlook.com>
2019-06-16 22:39:08 +0300

    php unit test template added

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ echo "test"
 mysql --defaults-extra-file=mysqlParameters myapp_test -e "$(cat mysqlScript)"
 echo "mysql script was executed"
 cat mysqlScript
+
+phpunit unitTest.php
---
+++ unitTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class StackTest extends PHPUnit_Framework_TestCase
+{
+    public function testPushAndPop()
+    {
+        $stack = [];
+        $this->assertSame(0, count($stack));
+
+        array_push($stack, 'foo2');
+        $this->assertSame('foo', $stack[count($stack)-1]);
+        $this->assertSame(1, count($stack));
+
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertSame(0, count($stack));
+    }
+}
+?>
