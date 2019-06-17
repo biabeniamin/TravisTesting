dcd4f2a7176671cb08793b8e216ae70ee3ba6bd7
Beniamin Bia <biabeniamin@outlook.com>
2019-06-17 21:19:21 +0300

    get accounts from db

--- build.sh
+++ build.sh
@@ -12,8 +12,12 @@ sudo apt-get install php-mysql -y
 sudo service apache2 restart
 sudo rm /var/www/html/index.html
 sudo cp index.php /var/www/html/index.php
+sudo cp Accounts.php /var/www/html/Accounts.php
+sudo cp Models /var/www/html/. -r
+
 
 echo "getting messagfe from server"
 curl localhost
+curl localhost/Accounts.php?cmd=getAccounts
 
 echo "done getting message from server"
