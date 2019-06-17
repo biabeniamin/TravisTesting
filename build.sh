ae96a80bd8d424ac4fa555e4a6a54b7b0e9c60c9
Beniamin Bia <biabeniamin@outlook.com>
2019-06-17 10:04:05 +0300

    index file added to apache folder

--- build.sh
+++ build.sh
@@ -8,5 +8,7 @@ cat mysqlScript
 
 sudo apt install apache2
 apt-get install php5 php-pear php5-mysql
+sudo rm /var/www/html/index.html
+sudo cp index.php /var/www/html/index.php
 wget localhost
 cat index.html
